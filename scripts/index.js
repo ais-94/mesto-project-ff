0dd65b111d2b6ef7323a1173d2a8906545438611
Aysylu Tuityrova <ais-94@yandex.ru>
2024-10-27 21:51:29 +0300

    after correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template");
 
 // @todo: DOM узлы
+let listSectionUl = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(item, handleDelete) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+  cardElement.querySelector("img").src = item.link;
+  cardElement.querySelector("img").alt = item.name; //Alt
+  cardElement.querySelector("h2").textContent = item.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", handleDelete);
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function cardDelete(evt) {
+  const listItem = evt.target.closest(".places__item");
+  listItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
-
-const contain = document.querySelector(".content"); // находим main
-const section = contain.querySelector(".page__section"); // находим секцию с кнопкой
-const button = section.querySelector(".profile__add-button"); //кнопка
-const page = document.querySelector(".page__content"); //page
-const addButton = page.querySelector(".popup_type_new-card"); //*всплывающее окно
-const closebutton = addButton.querySelector(".popup__close");
-const savecardbutton = addButton.querySelector(".popup__button"); //содержимое всплывающего окна
-const template = document.querySelector("#card-template");
-const li_template = template.querySelector("li");
-
-function addCardElement(element) {
-  const page = document.querySelector(".page__content");
-  const list_section = page.querySelector(".places");
-  const list_section_ul = list_section.querySelector(".places__list");
-  const template = document.querySelector("#card-template");
-
-  // Клонируем содержимое тега <template>
-  const item = template.content.cloneNode(true);
-
-  // Находим тег <li> и помещаем текст внутрь
-  const list_temp = item.querySelector("li");
-  list_temp.querySelector("img").src = element.link;
-  list_temp.querySelector("h2").textContent = element.name;
-  list_section_ul.append(item);
-  const deletecard = list_temp.querySelector(".card__delete-button");
-  deletecard.addEventListener("click", deleteCard);
-}
-//Вывести карточки из массива на страницу
-initialCards.forEach(addCardElement);
-
-//Функция создания карточки
-function addcard(e) {
-  const name = document.querySelector(".popup__input_type_card-name").value;
-  const link = document.querySelector(".popup__input_type_url").value;
-
-  if (name && link) {
-    e.preventDefault();
-
-    const element = {
-      name: name,
-      link: link,
-    };
-
-    addCardElement(element);
-    popup_close();
-  }
-}
-
-//Функция открыть окно
-function popup_open() {
-  addButton.style.display = "flex";
-}
-
-//Функция закрыть окно
-function popup_close() {
-  addButton.style.display = "none";
-}
-
-//Клик открыть окно
-button.addEventListener("click", function () {
-  popup_open();
+initialCards.forEach(function (element) {
+  const cardItem = createCard(element, cardDelete);
+  listSectionUl.prepend(cardItem);
 });
-
-//Клик закрыть окно
-closebutton.addEventListener("click", function () {
-  popup_close();
-});
-
-savecardbutton.addEventListener("click", addcard);
-
-//Удаление карточки
-function deleteCard() {
-  const revDiv = this.parentElement;
-  revDiv.remove();
-}
